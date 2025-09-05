f3ac34d26f9e732ac0ae0cfbb2c3f557c1a4b2cc
Zarro. <arunez.zro@gmail.com>
2025-09-05 09:08:43 +0530

    #86 working on LMS

---
+++ apps/lms/app.ts
@@ -0,0 +1,6 @@
+import type { RouteRegistery } from '../../cortex/http/route_registery';
+import { lmsRoutes} from './src/code/route';
+
+export async function registerApp(registry: RouteRegistery) {
+    registry.addProvider(lmsRoutes);
+}
